fcdd56406efb27c2a4a931307e1428d7647e99fd
Sede Soukossi <4968379+issokuos@users.noreply.github.com>
2026-03-04 01:02:30 +0100

    Fix docs crate attribute

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! The fundamental invariant is that for a given idempotency key, the side effect must be executed **at most once**,
 //! and the response MUST be returned **at least once** (to every request bearing that key within the TTL window)
-#![cfg_attr(docsrs, doc(cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 pub mod config;
 pub mod entry;
